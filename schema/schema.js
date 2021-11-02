e2aa53b81ad47a785e496d5aea8c1a32b3d8c115
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-11-02 12:30:45 -0700

    create rootquery, add user that return dummy data, and export rootquery

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,19 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+// dummy data
+const users = [
+  { id: "23", firstName: "Bart", age: 20 },
+  { id: "21", firstName: "Kang", age: 18 }
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -9,4 +22,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLID } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
