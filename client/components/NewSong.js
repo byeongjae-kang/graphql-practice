08892388240ca34d0f3060d0e8375d3184ea2e90
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-11-04 00:06:58 -0700

    create basic form to start

---
+++ client/components/NewSong.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewSong = () => {
+  return (
+    <form>
+      <label>Title</label>
+      <input type="text" name="title" />
+    </form>
+  );
+};
+
+export default NewSong;
