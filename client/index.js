f7f14b851f2f086a0c767485990fad4d33db76b9
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-11-04 00:06:10 -0700

    setup react router dom, import layout and render

--- client/index.js
+++ client/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+import Layout from "./layout/Layout";
 import Container from "./components/Container";
 import SongList from "./components/SongList";
+import NewSong from "./components/NewSong";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -16,8 +18,12 @@ const App = () => {
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Container />}>
-            <Route index element={<SongList />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Container />}>
+              <Route index element={<SongList />} />
+              <Route path="songs" element={<SongList />} />
+              <Route path="songs/new" element={<NewSong />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
