d43e11080f06166c802d985f5ea88a1c4000fa7d
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-11-04 20:28:37 -0700

    add new song, reset input, and redirect to songs

--- client/components/NewSong.js
+++ client/components/NewSong.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+
+const ADD_SONG = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
 
 const NewSong = () => {
+  const [addSong, { loading, error, data }] = useMutation(ADD_SONG);
+  const [title, setTitle] = useState("");
+  const navigate = useNavigate();
+
+  if (loading) {
+    return <p>adding the song...</p>;
+  }
+  if (error) {
+    return <p>Error :(</p>;
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    addSong({ variables: { title: title } });
+
+    setTitle("");
+    navigate("/songs");
+  };
+
   return (
-    <form>
-      <label>Title</label>
-      <input type="text" name="title" />
-    </form>
+    <div>
+      <h3>Create New Song</h3>
+      <form onSubmit={submitHandler}>
+        <label>Song Title:</label>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
